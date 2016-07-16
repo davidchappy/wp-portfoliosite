c2c5ef79520c75aec04a83304dac6169241da50d
David Chapman <davidchappy@gmail.com>
2016-07-16 15:25:28 -0400

    Revert "Minor JS and CSS changes"
    This reverts commit f991335d6a8a6f7974819d4f5016d8dd57b9647a.

--- wp-content/themes/dac-portfolio/js/scripts.js
+++ wp-content/themes/dac-portfolio/js/scripts.js
@@ -1,13 +1,8 @@
 jQuery(document).ready(function($) {
-	
-	// Fade-in effect
 	$(function() {
 	    $('body').removeClass('fade-out');
 	});
 	$(document).ready(function() {
 		$('.headshot').css('opacity', '1');
 	});
-
-	
-
 });
